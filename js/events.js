befa66c5f86dcdcfec95c25443a926c9055d6f2b
César Carruitero <ccarruitero@protonmail.com>
2019-03-04 12:30:04 -0500

    add more event details

--- js/events.js
+++ js/events.js
@@ -1,15 +1,27 @@
 const userId = '';
 const token = '';
 
+const getElement = (clone, tag) => {
+  return clone.querySelector(`.event-${tag}`);
+};
+
+const addText = (clone, tag, content) => {
+  getElement(clone, tag).textContent = content;
+};
+
 const events = ({ data }) => {
   data.forEach(event => {
     let wrapper = document.querySelector('#events-wrapper');
     let template = document.querySelector('#event-box-template');
     let clone = template.content.cloneNode(true);
-    let title = clone.firstElementChild;
-    title.textContent = event.name;
-    let time = clone.querySelector('.event-time');
-    time.textContent = `${event.local_date} - ${event.local_time}`;
+    addText(clone, 'title', event.name);
+    let timeContent = `${event.local_date} - ${event.local_time}`;
+    addText(clone, 'time', timeContent);
+    getElement(clone, 'desc').innerHTML = event.description;
+    getElement(clone, 'rsvp').setAttribute('href', event.link);
+    if (event.state === 'upcoming') {
+      addText(clone, 'rsvp', 'register in meetup');
+    }
     wrapper.appendChild(clone);
   });
 };
