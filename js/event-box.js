7a4638a85b3c2d6cfdead73aa44e4522cc8e5f1a
César Carruitero <ccarruitero@protonmail.com>
2019-09-17 20:15:05 -0500

    add templates inside component so can reuse in other pages

--- js/event-box.js
+++ js/event-box.js
@@ -1,11 +1,20 @@
 export default class extends HTMLElement {
+  static get observedAttributes() {
+    return ['event']
+  }
+
   constructor() {
     super();
-    const template = document
-      .getElementById('event-box-template')
-      .content;
+    const event = this.event;
+    const template = document.createElement('div');
+    template.innerHTML = `
+      <h3 class='event-title'>${event.name}</h3>
+      <div class='event-time'></div>
+      <a class='event-rsvp'>see in meetup</a>
+      <div class='event-desc'></div>
+    `;
     const shadow = this.attachShadow({mode: 'open'});
-    shadow.appendChild(template.cloneNode(true));
+    shadow.appendChild(template);
 
     const style = document.createElement('style');
     style.textContent = `
@@ -23,5 +32,55 @@ export default class extends HTMLElement {
     `;
 
     shadow.appendChild(style);
+  }
+
+  get event() {
+    const event = this.getAttribute('event');
+    return event ? JSON.parse(event) : {};
+  }
+
+  getChild(prefix, tag) {
+    return this.shadowRoot.querySelector(`.${prefix}-${tag}`);
+  }
+
+  addText(prefix, tag, content) {
+    this.getChild(prefix, tag).textContent = content;
+  }
+
+  concatAddress(venue) {
+    return `${venue.address_1}, ${venue.address_2}, ${venue.city}, ${venue.localized_country_name}`;
+  }
+
+  renderMap(lat, lon) {
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic3BsYXNoIiwiYSI6Ijc1RjlLUkEifQ.bYkfQEnsZfJWlX5DkAOz3g';
+    let map = L.mapbox.map('venue-map')
+      .setView([lat, lon], 17)
+      .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/light-v10'));
+
+    L.marker([lat, lon]).addTo(map);
+  }
+
+  attributeChangedCallback() {
+    const e = this.event;
+    this.addText('event', 'title', e.name);
+    let timeContent = `${e.local_date} ${e.local_time}`;
+    this.addText('event', 'time', timeContent);
+    this.getChild('event', 'desc').innerHTML = e.description;
+    this.getChild('event', 'rsvp').setAttribute('href', e.link);
+
+    if (e.status == 'upcoming') {
+      this.addText('event', 'rsvp', 'register in meetup');
+      const venue = e.venue;
+      const venueTemplate = document.createElement('div');
+      venueTemplate.innerHTML = `
+        <h3>Venue</h3>
+        <div class='venue-info'>
+          <div class='venue-name'>${venue.name}</div>
+          <div class='venue-address'>${this.concatAddress(venue)}</div>
+        </div>
+      `;
+      this.shadowRoot.appendChild(venueTemplate);
+      this.renderMap(venue.lat, venue.lon);
+    }
   }
 }
