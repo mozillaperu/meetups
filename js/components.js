6a40a300a2e08f1b8757421a6ab42aead08c3b95
César Carruitero <ccarruitero@protonmail.com>
2019-11-27 12:56:59 -0500

    update social link component

--- js/components.js
+++ js/components.js
@@ -4,4 +4,4 @@ import socialLink from './social-link.js';
 
 customElements.define('twitter-box', twitterBox);
 customElements.define('event-box', eventBox);
-customElements.define('social-link', socialLink, { extends: 'p' });
+customElements.define('social-link', socialLink);
