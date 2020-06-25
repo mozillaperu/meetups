5847905f5c6cbc6d638c47cd3480d4927222d694
César Carruitero <ccarruitero@protonmail.com>
2020-06-25 01:28:59 -0500

    get twitter image from api for presentation

--- js/twitter-box.js
+++ js/twitter-box.js
@@ -1,34 +1,40 @@
 export default class extends HTMLElement {
   constructor() {
     super()
-    const twitter = this.twitter;
-    const box = document.createElement('div');
-    box.innerHTML = `
-      <small>
-        <a href='https://twitter.com/${twitter}'>
-          <img src='https://twitter.com/${twitter}/profile_image?size=original' />
-          <span><i class='fab fa-twitter'></i><span>
-          @${twitter}
-        </a>
-      </small>
-      <style>
-        img {
-          width: 200px;
-          border-radius: 50%;
-          background: grey;
-        }
+    const url = `https://mozpe-api.vercel.app/api/twitter-user?name=${this.twitter}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(({ profile_image_url_https }) => {
+        const twitter = this.twitter;
+        const image = profile_image_url_https.replace('_normal', '');
+        const box = document.createElement('div');
+        box.innerHTML = `
+          <small>
+            <a href='https://twitter.com/${twitter}'>
+              <img src='${image}' />
+              <span><i class='fab fa-twitter'></i><span>
+              @${twitter}
+            </a>
+          </small>
+          <style>
+            img {
+              width: 200px;
+              border-radius: 50%;
+              background: grey;
+            }
 
-        a {
-          text-decoration: none;
-          display: block;
-          color: #000;
-          padding: 15px;
-          font-weight: 700;
-        }
-      </style>
-    `;
-   const shadow = this.attachShadow({ mode: 'open' });
-   shadow.appendChild(box);
+            a {
+              text-decoration: none;
+              display: block;
+              color: #000;
+              padding: 15px;
+              font-weight: 700;
+            }
+          </style>
+        `;
+       const shadow = this.attachShadow({ mode: 'open' });
+       shadow.appendChild(box);
+     });
   }
 
   get twitter() {
